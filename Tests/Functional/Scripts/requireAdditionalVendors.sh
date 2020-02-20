770dd2cc6480b925f561ce022130c365a50dda92
Christian Alexander Wolf <mail@cawolf.de>
2020-02-20 11:09:00 +0100

    fix: warmup zipkin container

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -5,4 +5,6 @@ composer remove auxmoney/opentracing-bundle-jaeger
 composer require auxmoney/opentracing-bundle-zipkin:dev-${BRANCH}
 cd ../../
 
-docker create -p 9411:9411 --name zipkin openzipkin/zipkin:2.19
+docker run -d -p 9411:9411 --name zipkin openzipkin/zipkin:2.19
+sleep 5;
+docker stop zipkin
