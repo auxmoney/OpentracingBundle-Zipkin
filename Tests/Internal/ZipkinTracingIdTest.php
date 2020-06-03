5c03130286b5db2a0df1c7e600216a60fa5d3c8e
Christian Alexander Wolf <cawolf@users.noreply.github.com>
2020-06-03 10:54:06 +0200

    feat: add trace id header capability (#13)
    contributors: @mmross

---
+++ Tests/Internal/ZipkinTracingIdTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Auxmoney\OpentracingBundle\Tests\Internal;
+
+use Auxmoney\OpentracingBundle\Internal\ZipkinTracingId;
+use Auxmoney\OpentracingBundle\Service\Tracing;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Prophecy\ObjectProphecy;
+
+class ZipkinTracingIdTest extends TestCase
+{
+    /** @var Tracing|ObjectProphecy */
+    private $tracing;
+    /** @var ZipkinTracingId */
+    private $subject;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->tracing = $this->prophesize(Tracing::class);
+
+        $this->subject = new ZipkinTracingId($this->tracing->reveal());
+    }
+
+    public function testGetAsStringSuccess(): void
+    {
+        $this->tracing->injectTracingHeadersIntoCarrier([])->shouldBeCalled()->willReturn(['x-b3-traceid' => 'abc']);
+
+        self::assertSame('abc', $this->subject->getAsString());
+    }
+
+    public function testGetAsStringNoHeader(): void
+    {
+        $this->tracing->injectTracingHeadersIntoCarrier([])->shouldBeCalled()->willReturn([]);
+
+        self::assertSame('none', $this->subject->getAsString());
+    }
+}
