1503dd4abc96a849a61c7ec4744d7988d656206a
Christian A. Wolf <mail@cawolf.de>
2020-12-24 11:32:15 +0100

    replace travis with github actions

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+shopt -s extglob
 
 cd build/testproject/
 composer remove auxmoney/opentracing-bundle-jaeger
-composer config repositories.origin vcs https://github.com/${PR_ORIGIN}
-composer require auxmoney/opentracing-bundle-zipkin:dev-${BRANCH}
+composer require auxmoney/opentracing-bundle-zipkin
+rm -fr vendor/auxmoney/opentracing-bundle-zipkin/*
+cp -r ../../!(build|vendor) vendor/auxmoney/opentracing-bundle-zipkin
+composer dump-autoload
 cd ../../
 
 docker run -d -p 9411:9411 --name zipkin openzipkin/zipkin:2.19
