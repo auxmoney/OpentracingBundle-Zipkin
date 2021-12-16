25355b77c2570066f1220d91bc7de9cdbafbc92c
Christian A. Wolf <mail@cawolf.de>
2021-12-16 08:14:36 +0100

    fix: default for expansion

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 shopt -s extglob
 
-echo "DEBUG: GITHUB_HEAD_REF = $GITHUB_HEAD_REF"
-echo "DEBUG: GITHUB_REF = $GITHUB_REF"
-
 cd build/testproject/
 composer remove auxmoney/opentracing-bundle-jaeger
 VENDOR_VERSION=""
-CURRENT_REF=${GITHUB_HEAD_REF:-GITHUB_REF}
+CURRENT_REF=${GITHUB_HEAD_REF:-$GITHUB_REF}
 CURRENT_BRANCH=${CURRENT_REF#refs/heads/}
-echo "DEBUG: CURRENT_REF = $CURRENT_REF"
-echo "DEBUG: CURRENT_BRANCH = $CURRENT_BRANCH"
 if [[ $CURRENT_BRANCH -ne "master" ]]; then
     composer config minimum-stability dev
     VENDOR_VERSION=":dev-${CURRENT_BRANCH}"
